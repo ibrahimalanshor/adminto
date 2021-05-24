3c16b591943efc99d01745c0d2ad3f9b662592ee
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-05-24 20:30:25 +0700

    Add Outside CLick & whitelist tailwind

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -8,8 +8,18 @@ const openDropdown = function (e) {
   dropdownMenuClass.toggle('block')
 }
 
+const outsideClick = (target, dropdown, toggle) => {
+  if (!dropdown.contains(target) && target !== toggle) {
+    dropdown.querySelector('.dropdown-menu').classList.remove('block')
+  }
+}
+
 dropdowns.forEach(dropdown => {
   const toggle = dropdown.querySelector('.dropdown-toggle')
+
+  window.addEventListener('click', e => {
+    outsideClick(e.target, dropdown, toggle)
+  })
 
   toggle.onclick = openDropdown
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  purge: [
-    './src/**/*.njk'
-  ],
+  purge: {
+   content: ['./src/**/*.njk'],
+   options: {
+    whiteListPatterns: [/^text-/, /^bg-/, /^w-/, /^sm:w-/, /^md:w-/, /^lg:w-/, /^xl:w-/, , /^2xl:w-/]
+   } 
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
