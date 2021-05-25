701b3be26dab87b189bfc40e27be34771ac51090
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-05-25 08:33:16 +0700

    Fix Purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: {
    content: ['./src/**/*.njk'],
    options: {
-    whiteListPatterns: [/^text-/, /^bg-/, /^w-/, /^sm:w-/, /^md:w-/, /^lg:w-/, /^xl:w-/, , /^2xl:w-/]
+    safelist: { standard: [/^text-/, /^bg-/, /^w-/, /^sm:w-/, /^md:w-/, /^lg:w-/, /^xl:w-/, , /^2xl:w-/] }
    } 
   },
   darkMode: false, // or 'media' or 'class'
