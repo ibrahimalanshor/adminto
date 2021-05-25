ffef395698d49e7a3c4e9ad183acfe085bef2f57
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-05-25 19:42:43 +0700

    Fix Regex Purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: {
    content: ['./src/**/*.njk'],
    options: {
-    safelist: { standard: [/^text-/, /^bg-/, /^w-/, /^sm:w-/, /^md:w-/, /^lg:w-/, /^xl:w-/, , /^2xl:w-/] }
+    safelist: { standard: [
+      /^(text|bg)-(red|green|blue|gray|yellow)/,
+      /^(w-|sm:w-|md:w-|lg:w-|xl:w-).*[/][4]$/,
+      /^(m|p)(y|x|t|l|r|b)-[1-4]$/,
+      /^(m|p)-[1-4]$/,
+    ] }
    } 
   },
   darkMode: false, // or 'media' or 'class'
